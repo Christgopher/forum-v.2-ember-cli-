6379ca8d378308c0497556364670ea845bd11a9d
chris le compte <student@epicodus.com>
2015-06-19 10:19:55 -0700

    add questions to app

---
+++ app/controllers/new-question.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    actions: {
+   save: function() {
+     var newQuestion = this.store.createRecord('question', {
+       question: this.get('question'),
+       author: this.get('author'),
+       description: this.get('description')
+     });
+     newQuestion.save();
+     this.transitionToRoute('questions');
+   }
+ }
+});
--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'forum-app',
     environment: environment,
+    firebase: 'glowing-fire-8249.firebaseIO.com',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
